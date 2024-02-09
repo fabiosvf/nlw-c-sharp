8671b0cf8f9ef678068df50275919b3fd9387d46
Fabio Silva Ferreira <fabiosvf@gmail.com>
2024-02-08 21:40:15 -0300

    NLW 14 - C#

---
+++ src/RocketseatAuction.API/Filters/AuthenticationUserAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Repositories;
+
+namespace RocketseatAuction.API.Filters;
+
+public class AuthenticationUserAttribute : AuthorizeAttribute, IAuthorizationFilter
+{
+    private IUserRepository _repository;
+    public AuthenticationUserAttribute(IUserRepository repository) => _repository = repository; 
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        try
+        {
+            var token = TokenOnRequest(context.HttpContext);
+
+            var email = FromBase64String(token);
+
+            var exists = _repository.ExistUserWithEmail(email);
+
+            if (!exists)
+                context.Result = new UnauthorizedObjectResult("E-mail not valid");
+        }
+        catch(Exception ex)
+        {
+            context.Result = new UnauthorizedObjectResult(ex.Message);
+        }
+    }
+
+    private string TokenOnRequest(HttpContext context)
+    {
+        var authentication = context.Request.Headers.Authorization.ToString();
+
+        if (string.IsNullOrEmpty(authentication))
+        {
+            throw new Exception("Token is missing.");
+        }
+
+        return authentication["Bearer ".Length..];
+    }
+
+    private string FromBase64String(string base64)
+    {
+        var data = Convert.FromBase64String(base64);
+
+        return System.Text.Encoding.UTF8.GetString(data);
+    }
+}
